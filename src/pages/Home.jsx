c800080fa09c9852698e48d3cbbf56421af4aaca
Brahim Knioui <brahimknioui@Brahims-MacBook-Air.local>
2024-09-28 20:02:28 +0300

    carousel videos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,4 @@
-
-
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 const HomeWrapper = styled.div`
   text-align: center;
@@ -10,78 +7,69 @@ const HomeWrapper = styled.div`
   padding: 50px;
 `;
 
-
-;
-
 function Home() {
   return (
-    <HomeWrapper style={{backgroundImage: `url("/assets/backgroundabout.jpg")`, backgroundRepeat:"no-repeat"}}>   
-      <h1 className='text-light'>مرحبا بكم في غسيل السيارات</h1>
-
-<div>
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide "></button>
+    <HomeWrapper
+      style={{
+        backgroundImage: `url("/assets/backgroundabout.jpg")`,
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <h1 className="text-light">مرحبا بكم في غسيل السيارات</h1>
+      <div className="container-fluid">
+     
+        <div id="carouselExample" className="carousel slide ">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+    <video w-100 src="/assets/service2.mp4" controls></video>
+    </div>
+    <div className="carousel-item">
+    <video w-100 src="/assets/service2.mp4" controls></video>
+    </div>
+    <div className="carousel-item">
+    <video w-100 src="/assets/service2.mp4" controls></video>
+    </div>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="/assets/manTube.webp" class="d-block w-100" alt="manwasing car with tube "/>
-      <div class="carousel-caption d-none d-md-block">
-      <h5>غسيل خارجي وداخلي شامل</h5>
-      
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="/assets/washcar.webp" class="d-block w-100" alt="man waxing car"/>
-      <div class="carousel-caption d-none d-md-block">
-      <h5>تلميع وتفصيل السيارة باستخدام أفضل المنتجات</h5>
-      
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="/assets/icon3.webp" class="d-block w-100" alt="vacumming "/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5> إزالة الأوساخ العنيدة والبقع</h5>
-       
-      </div>
-    </div>
-  
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
   </button>
 </div>
+        
+      </div>
 
-</div>
-
-<h2 className='text-light mt-3'>خدماتنا</h2>
-      <div className="row   m-0 ">
-        <div className="col-lg-3 col-md-6 m-auto "><img className='w-50  m-2' src="/assets/manTube.webp" alt="" />
-        <br />
-        <h5>غسيل خارجي وداخلي شامل</h5>
+      <h2 className="text-light mt-3">خدماتنا</h2>
+      <div className="row m-0 ">
+        <div className="col-lg-3 col-md-6 m-auto ">
+          <img className="w-50  m-2" src="/assets/manTube.webp" alt="" />
+          <br />
+          <h5>غسيل خارجي وداخلي شامل</h5>
         </div>
-        <div className="col-lg-3 col-md-6 m-auto"><img className='w-50 m-2' src="/assets/washcar.webp" alt="" />
-        <br />
-        <p>إزالة الأوساخ العنيدة والبقع</p></div>
-        <div className="col-lg-3 col-md-6 m-auto"><img className='w-50 m-2' src="/assets/icon3.webp" alt="" />
-        <br />
-        <p>تلميع وتفصيل السيارة باستخدام أفضل المنتجات</p></div>
-        <div className="col-lg-3 col-md-6 m-auto"><img className='w-50 m-2' src="/assets/icon4.webp" alt="" />
-        <br />
-        <p>خدمات إضافية مثل تنظيف المقاعد والفرش.</p></div>
-
+        <div className="col-lg-3 col-md-6 m-auto">
+          <img className="w-50 m-2" src="/assets/washcar.webp" alt="" />
+          <br />
+          <p>إزالة الأوساخ العنيدة والبقع</p>
+        </div>
+        <div className="col-lg-3 col-md-6 m-auto">
+          <img className="w-50 m-2" src="/assets/icon3.webp" alt="" />
+          <br />
+          <p>تلميع وتفصيل السيارة باستخدام أفضل المنتجات</p>
+        </div>
+        <div className="col-lg-3 col-md-6 m-auto">
+          <img className="w-50 m-2" src="/assets/icon4.webp" alt="" />
+          <br />
+          <p>خدمات إضافية مثل تنظيف المقاعد والفرش.</p>
+        </div>
       </div>
- 
-   
-      <p className='text-light fs-2'>نحن نقدم أفضل خدمات غسيل السيارات في المدينة! يمكنناأن نأتي إلى منزلك لغسل سيارتك</p>
+
+      <p className="text-light fs-2">
+        نحن نقدم أفضل خدمات غسيل السيارات في المدينة! يمكنناأن نأتي إلى منزلك
+        لغسل سيارتك
+      </p>
     </HomeWrapper>
   );
 }
