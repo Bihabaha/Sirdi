af564e67fd88f93c74f936af2facc2e6da3b60e7
Brahim Knioui <brahimknioui@Brahims-MacBook-Air.local>
2024-09-28 18:21:34 +0300

    hamburger menu color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Header() {
     <nav className="navbar navbar-expand-lg" style={{backgroundImage: `url("/assets/backgoundcarwash.jpg")`, backgroundRepeat:"no-repeat",backgroundPosition:"left", WebkitBackgroundSize:"cover"}}>
   <div className="container-fluid">
     <Link className="navbar-brand" to="/"><img  src={logo} alt="" /></Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
